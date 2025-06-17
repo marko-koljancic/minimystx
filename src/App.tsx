a5705159ac53228f611a12f9d442910f5011b35e
marko-koljancic <marko.koljancic@gmail.com>
2025-06-17 14:08:57 +0200

    Add @xyflow/react dependency and implement ReactFlow in App component; remove unused index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
-function App() {
-  return <p>Test...</p>;
-}
+import { Background, Controls, ReactFlow } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import "./App.css";
 
+const nodes = [
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { label: "Hello" },
+    type: "input",
+  },
+  {
+    id: "2",
+    position: { x: 100, y: 100 },
+    data: { label: "World" },
+  },
+];
+
+const edges = [{ id: "1-2", source: "1", target: "2" }];
+
+function App() {
+  return (
+    <div style={{ height: "100%" }}>
+      <ReactFlow nodes={nodes} edges={edges}>
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+}
 export default App;
