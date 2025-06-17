0266e775a2c88cf867751dde331ec724ddb0bcd3
marko-koljancic <marko.koljancic@gmail.com>
2025-06-17 14:24:00 +0200

    Refactor App component to use initialNodes and initialEdges; enhance ReactFlow integration with state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,63 @@
-import { Background, Controls, ReactFlow } from "@xyflow/react";
+import { useCallback } from "react";
+import {
+  addEdge,
+  Background,
+  Controls,
+  ReactFlow,
+  SelectionMode,
+  useEdgesState,
+  useNodesState,
+} from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import "./App.css";
 
-const nodes = [
+const initialNodes = [
   {
     id: "1",
-    position: { x: 0, y: 0 },
-    data: { label: "Hello" },
-    type: "input",
+    data: { label: "Node 1" },
+    position: { x: 150, y: 0 },
   },
   {
     id: "2",
-    position: { x: 100, y: 100 },
-    data: { label: "World" },
+    data: { label: "Node 2" },
+    position: { x: 0, y: 150 },
+  },
+  {
+    id: "3",
+    data: { label: "Node 3" },
+    position: { x: 300, y: 150 },
   },
 ];
 
-const edges = [{ id: "1-2", source: "1", target: "2" }];
+const initialEdges = [
+  { id: "e1-2", source: "1", target: "2" },
+  { id: "e1-3", source: "1", target: "3" },
+];
 
 function App() {
+  const [nodes, setNodes, onNodeChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
+  const panOnDrag = [1, 2];
+
   return (
     <div style={{ height: "100%" }}>
-      <ReactFlow nodes={nodes} edges={edges}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodeChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        panOnScroll
+        selectionOnDrag
+        panOnDrag={panOnDrag}
+        selectionMode={SelectionMode.Partial}
+      >
         <Background />
         <Controls />
       </ReactFlow>
