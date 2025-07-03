fb1de9ffab407f01d25525ba5ed1752c7641ebab
marko-koljancic <marko.koljancic@gmail.com>
2025-07-03 12:27:04 +0200

    feat: update dependencies and add WASM support
    - Added WASM build script to package.json.
    - Updated React and related dependencies to latest versions.
    - Removed unused CSS file (App.css).
    - Refactored App component to use React Router for navigation.
    - Removed ControlledFlow and UncontrolledFlow components.
    - Added new Header, Home, and NotFound components.
    - Created MainLayout component for consistent layout structure.
    - Added basic styles in index.css.
    - Initialized WASM engine with basic add function.
    - Added .gitignore and Cargo files for WASM engine.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import "./App.css";
-// import ControlledFlow from "./ControlledFlow";
-import UncontrolledFlow from "./UncontrolledFlow";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-function App() {
-  return <UncontrolledFlow />;
-}
+import MainLayout from "./layouts/MainLayout";
+import Home from "./components/home";
+import NotFound from "./components/not-found";
+import Header from "./components/header";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/design" element={<About />} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
+  );
+};
 
 export default App;
